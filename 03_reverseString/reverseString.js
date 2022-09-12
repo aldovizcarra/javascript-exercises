10e5c1070e382ca0ce30d744be44e723d17c73ce
Aldo Vizcarra <avizcarra87@gmail.com>
2022-09-12 00:19:59 -0700

    Complete javascript exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let text = string;
+  const myArr = text.split("");
+  text = "";
 
+  for (let i = myArr.length - 1; i >= 0; i--) {
+    text += myArr[i];
+  }
+
+  return text;
 };
 
 // Do not edit below this line
