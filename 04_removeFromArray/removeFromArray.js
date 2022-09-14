df9d8a66ce81ec201f108c256c5296084221f00c
Aldo Vizcarra <avizcarra87@gmail.com>
2022-09-14 00:11:20 -0700

    Complete js exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const arr1 = [];
 
+const removeFromArray = function (arr1, ...manyMoreArgs) {
+  outer: for (let i = 0; i < manyMoreArgs.length; i++) {
+    for (let j = 0; j < arr1.length; j++) {
+      if (manyMoreArgs[i] === arr1[j]) {
+        arr1.splice(j, 1);
+        continue outer;
+      }
+    }
+  }
+
+  return arr1;
 };
 
 // Do not edit below this line
