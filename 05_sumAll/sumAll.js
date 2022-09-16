c08811815b2b800531be188ce9ac9368588c9570
Aldo Vizcarra <avizcarra87@gmail.com>
2022-09-15 20:54:50 -0700

    Edit sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
 const sumAll = function (num1, num2) {
+  if (
+    num1 < 0 ||
+    num2 < 0 ||
+    num1 === num1.toString() ||
+    num2 === num2.toString() ||
+    isNaN(num1) ||
+    isNaN(num2)
+  ) {
+    return "ERROR";
+  } else if (num1 > num2) {
+    let param1 = num1;
+    num1 = num2;
+    num2 = param1;
+  }
+
   let sum = 0;
   for (let i = num1; i <= num2; i++) {
     sum += i;
