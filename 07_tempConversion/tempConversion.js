d4d8ccc8e6fe29dfd0583159e3648dc1b5b9eac3
Aldo Vizcarra <avizcarra87@gmail.com>
2022-09-17 12:57:35 -0700

    Complete temp conversion functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+  const celsius = ((temp - 32) * 5) / 9;
+  const roundedC = Number(celsius.toFixed(1));
+  return roundedC;
 };
 
-const ctof = function() {
-
+const ctof = function (temp) {
+  const fahrenheit = (temp * 9) / 5 + 32;
+  const roundedF = Number(fahrenheit.toFixed(1));
+  return roundedF;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
